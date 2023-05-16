9519b28896de792a795aa232dec24a06d5bf44ee
CJ <ngassajason@yahoo.fr>
2023-05-16 01:00:06 +0100

    ✨ adds crud for appointments

--- controllers/appointment.ctrl.js
+++ controllers/appointment.ctrl.js
@@ -1,5 +1,70 @@
+const db = require('../models');
+const Appointments = db.appointments;
 
+/**
+  * Create an Appointment.
+  * @param {*} req request object
+  * @param {*} res response object
+  * @returns {*} returns an error message with status code 500 and an error object 
+  *    {error: [String|Object]} or a response object of the created Appointment with status code 201
+  * */
+exports.createAppointment = async (req, res, next) => {
+  console.log('createAppointment');
+  console.log(req.body)
+  try{
+		const result = await Appointments.create({
+      ...req.body,
+		})
+		return res.status(201).json(result)
+	}catch (error) {
+		return res.status(500).json({ error: error ? error : new Error('Error Creating Appointment') })
+	}
+}
 
-exports.createAppointment = (req, res, next) => {
-  
+/**
+  * Get A List of all Appointments
+  * @param {*} req request object
+  * @param {*} res response object
+  * @returns {*} returns an error message with status code 500 and an error object 
+  *    {error: [String|Object]} or a response array of objects of the requested Appointments  with status code 200
+  * */
+exports.getAllAppointments = async (req, res) => {
+  try {
+    const appointments = await Appointments.findAll();
+    return res.status(200).json(appointments);
+  } catch (error) {
+		return res.status(500).json({ error: error ? error : new Error('Could not get your data') })
+  }
+}
+
+/**
+  * Delete an Appointment.
+  * @param {*} req request object
+  * @param {*} res response object
+  * @returns {*} returns an error message with status code 500 and an error object 
+  *    {error: [String|Object]} or a response  object {message: String} with status code 204
+  * */
+exports.deleteAppointment = async (req, res) => {
+	try{
+		await Appointments.destroy({where: {id: req.params.id}})
+		return res.status(204).json({message: 'Appointment Deleted Successfully'})
+	}catch(error){
+		return res.status(500).json({error: error ? error : new Error('Error Deleting Appointment')})
+	}
+}
+
+/**
+  * Edit an Appointment.
+  * @param {*} req request object
+  * @param {*} res response object
+  * @returns {*} returns an error message with status code 500 and an error object 
+  *    {error: [String|Object]} or a response  object {message: String} with status code 200
+  * */
+exports.editAppointment = async (req, res) => {
+	try{
+		await Appointments.update(req.body,{where: {id: req.params.id}})
+		return res.status(200).json({message: 'Appointment Updated Successfully'})
+	}catch(error){
+		return res.status(500).json({error: error ? error : new Error('Error Updating Appointment')})
+	}
 }
--- routes/appointment.route.js
+++ routes/appointment.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const appointmentCtrl = require('../controllers/appointment.ctrl');
 
 router.post('/new', appointmentCtrl.createAppointment);
-//router.post('/signup', userCtrl.signup);
+router.get('/', appointmentCtrl.getAllAppointments);
+router.put('/edit/:id', appointmentCtrl.editAppointment);
+router.delete('/delete/:id', appointmentCtrl.deleteAppointment);
 
 module.exports = router;
